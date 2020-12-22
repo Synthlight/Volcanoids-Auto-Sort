000da51c3830d4c80889cb0a018e2c7f1516566d
Gregory Brutsche <gregory@mail.brutsches.local>
2020-12-21 23:48:43 -0500

    Fix player inventory stack so damaged repair kits are sorted first.

--- Patches/InventoryControlPatch.cs
+++ Patches/InventoryControlPatch.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using BepInEx.Logging;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -32,16 +34,26 @@ namespace Auto_Sort.Patches {
                     }
                 }
 
-                //itemsToSort.Sort((item1, item2) => item1.itemDefinition.Category.SortOrder.CompareTo(item2.itemDefinition.Category.SortOrder));
                 itemsToSort.Sort((item1, item2) => {
-                    var item1Name = item1.itemDefinition.Name;
-                    var item2Name = item2.itemDefinition.Name;
+                    var item1Name = item1.itemDefinition.Name.Trim() + "::" + item1.itemDefinition.AssetId;
+                    var item2Name = item2.itemDefinition.Name.Trim() + "::" + item2.itemDefinition.AssetId;
+
+                    if (item1Name == item2Name && item1Name == "Module repair kit::1bf6d40a49d9e2148a2d2389a768f15f") {
+                        try {
+                            var healthStat = RuntimeAssetCache<CommonStats>.Instance.Health;
+                            var health1    = item1.propertySet.TryGet(healthStat, out PropertyValue health1Prop) ? health1Prop.GetValueOrDefault(PropertyType.Float, 1f) : 1f;
+                            var health2    = item2.propertySet.TryGet(healthStat, out PropertyValue health2Prop) ? health2Prop.GetValueOrDefault(PropertyType.Float, 1f) : 1f;
+
+                            return health1.CompareTo(health2);
+                        } catch (Exception e) {
+                            AutoSortMod.Log(LogLevel.Error, "Error sorting by durability:\n" + e);
+                        }
+                    }
 
                     return item1Name == item2Name ? 0 : AutoSortMod.itemSortOrders.TryGet(item1Name).CompareTo(AutoSortMod.itemSortOrders.TryGet(item2Name));
                 });
 
                 foreach (var item in itemsToSort) {
-                    //GMod.Log(LogLevel.Info, $"Adding {item.amount} \"{item.itemDefinition.Name}\".");
                     inventory.Add(null, item.itemDefinition, item.propertySet, item.amount, 0);
                     item.propertySet.Clear();
                 }
